afd55036aa0205f5ecd63af710328514152c20b1
Brandyn <bwhite@cs.umd.edu>
2010-06-10 20:52:50 -0400

    Pep8

--- base_test.py
+++ base_test.py
@@ -13,13 +13,15 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+"""Test
+"""
 __author__ = 'Brandyn A. White <bwhite@cs.umd.edu>'
 __license__ = 'GPL V3'
 
 import unittest
 
-class Test(unittest.TestCase):        
+
+class Test(unittest.TestCase):
     def test_add(self):
         self.assertEqual(1 + 1, 2)
 
